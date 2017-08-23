54181e7f0bba2947aa5683c72188635332a56a76
高思捷 <jahanngauss414@gmail.com>
2017-08-23 10:11:04 +0800

    test

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
 
-#check OS
+#check OS#
 if [ -f /etc/redhat-release ];then 
  OS='CentOS' 
  elif [ ! -z "`cat /etc/issue | grep bian`" ];then 
@@ -13,7 +13,7 @@ if [ -f /etc/redhat-release ];then
  exit 1 
  fi 
  
-#install
+#install#
  if [[ ${OS} == 'CentOS' ]];then 
  yum update -y
  yum install curl wget unzip ntp ntpdate lrzsz git screen -y 
@@ -22,7 +22,7 @@ if [ -f /etc/redhat-release ];then
  apt-get install unzip ntp ntpdate lrzsz git screen -y 
  fi 
 
- #Set DNS 
+ #Set DNS #
  echo "nameserver 8.8.8.8" > /etc/resolv.conf 
  echo "nameserver 8.8.4.4" >> /etc/resolv.conf 
  
