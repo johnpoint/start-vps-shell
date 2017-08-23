88f5dc51be4948d965b7cccc659ec53ad626f5fa
johnpoint <jahanngauss414@gmail.com>
2017-08-23 22:55:30 +0800

    fix

--- update-auto.sh
+++ update-auto.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
  export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
  
+ cd /home/wwwroot/github.lvcshu.tk
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/start.sh
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/v2ray.sh
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/install.sh
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/README.md
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/authorized_keys
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/ssh_key.sh
-  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/update-auto.sh
+ wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/update-auto.sh
