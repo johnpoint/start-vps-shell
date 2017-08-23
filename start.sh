4f982bcae443edee019c17ae9031f4b3fbda8b02
johnpoint <jahanngauss414@gmail.com>
2017-08-23 14:48:23 +0800

    test

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
-export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
+#=================================================
+#	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
+#	Version: 1.1.2
+#	Author: johnpoint
+#=================================================
+
+sh_ver=1.1.2
+
+check_root(){
+	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
+}
 #check OS#
 if [ -f /etc/redhat-release ];then 
  OS='CentOS' 
@@ -12,90 +24,85 @@ if [ -f /etc/redhat-release ];then
  echo "Not support OS, Please reinstall OS and retry!" 
  exit 1 
  fi 
- #menu
-check_sys
+ 
+ check_sys
 [[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] && [[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
 action=$1
 if [[ "${action}" == "clearall" ]]; then
 	Clear_transfer_all
 else
-	echo -e "  VPS一键管理脚本 ${Red_font_prefix}
-  ---- johnpoint ----
-  ${Green_font_prefix}1.${Font_color_suffix} 安装 软件
-  ${Green_font_prefix}2.${Font_color_suffix} 修改 密码
-  ${Green_font_prefix}3.${Font_color_suffix} 
-  ${Green_font_prefix}4.${Font_color_suffix} 
-  ${Green_font_prefix}5.${Font_color_suffix} 
-  ${Green_font_prefix}6.${Font_color_suffix} 
-  ${Green_font_prefix}7.${Font_color_suffix} 
-  ${Green_font_prefix}8.${Font_color_suffix} 
-  ${Green_font_prefix}9.${Font_color_suffix} 
-  ${Green_font_prefix}10.${Font_color_suffix} 
-  ${Green_font_prefix}11.${Font_color_suffix} 
-  ${Green_font_prefix}12.${Font_color_suffix} 
-  ${Green_font_prefix}13.${Font_color_suffix} 
-  ${Green_font_prefix}14.${Font_color_suffix} 
-  ${Green_font_prefix}15.${Font_color_suffix} 
+	echo -e "  ShadowsocksR MuJSON一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+  ---- Toyo | doub.io/ss-jc60 ----
+  ${Green_font_prefix}1.${Font_color_suffix} 安装 ShadowsocksR
+  ${Green_font_prefix}2.${Font_color_suffix} 更新 ShadowsocksR
+  ${Green_font_prefix}3.${Font_color_suffix} 卸载 ShadowsocksR
+  ${Green_font_prefix}4.${Font_color_suffix} 安装 libsodium(chacha20)
+————————————
+  ${Green_font_prefix}5.${Font_color_suffix} 查看 账号信息
+  ${Green_font_prefix}6.${Font_color_suffix} 显示 连接信息
+  ${Green_font_prefix}7.${Font_color_suffix} 设置 用户配置
+  ${Green_font_prefix}8.${Font_color_suffix} 手动 修改配置
+  ${Green_font_prefix}9.${Font_color_suffix} 清零 已用流量
+————————————
+ ${Green_font_prefix}10.${Font_color_suffix} 启动 ShadowsocksR
+ ${Green_font_prefix}11.${Font_color_suffix} 停止 ShadowsocksR
+ ${Green_font_prefix}12.${Font_color_suffix} 重启 ShadowsocksR
+ ${Green_font_prefix}13.${Font_color_suffix} 查看 ShadowsocksR 日志
+————————————
+ ${Green_font_prefix}14.${Font_color_suffix} 其他功能
+ ${Green_font_prefix}15.${Font_color_suffix} 升级脚本
  "
 	menu_status
 	echo && stty erase '^H' && read -p "请输入数字 [1-15]：" num
 case "$num" in
 	1)
-	Install
+	Install_SSR
 	;;
 	2)
-	cg_passwd
+	Update_SSR
 	;;
 	3)
-	
+	Uninstall_SSR
 	;;
 	4)
-	
+	Install_Libsodium
 	;;
 	5)
-	
+	View_User
 	;;
 	6)
-	
+	View_user_connection_info
 	;;
 	7)
-	
+	Modify_Config
 	;;
 	8)
-	
+	Manually_Modify_Config
 	;;
 	9)
-	
+	Clear_transfer
 	;;
 	10)
-	
+	Start_SSR
 	;;
 	11)
-	
+	Stop_SSR
 	;;
 	12)
-	
+	Restart_SSR
 	;;
 	13)
-	
+	View_Log
 	;;
 	14)
-	
+	Other_functions
 	;;
 	15)
-	
+	Update_Shell
 	;;
 	*)
 	echo -e "${Error} 请输入正确的数字 [1-15]"
 	;;
 esac
 fi
-#Install
-Install(){
-wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/install.sh && chmod install.sh && ./install.sh
-}
-#cg_passwd
-cg_passwd(){
-passwd
-}
 
