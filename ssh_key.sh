62f7e69ee935dfad5f5e1eff8a1c43cd4b2eed91
高思捷 <jahanngauss414@gmail.com>
2017-08-23 10:25:35 +0800

    1

--- ssh_key.sh
+++ ssh_key.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
-cd ~
-yum update -y
-yum install openssl -y
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
 
-mkdir .ssh
-cd ~/.ssh
-wget -N --no-check-certificate https://github.com/johnpoint/start-vps-shell/blob/master/authorized_keys
-chmod 600 authorized_keys
-chmod 700 ~/.ssh
+#check OS#
+if [ -f /etc/redhat-release ];then 
+ OS='CentOS' 
+ elif [ ! -z "`cat /etc/issue | grep bian`" ];then 
+ OS='Debian' 
+ elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then 
+ OS='Ubuntu' 
+ else 
+ echo "Not support OS, Please reinstall OS and retry!" 
+ exit 1 
+ fi 
+ 
+ #install#
+ if [[ ${OS} == 'CentOS' ]];then 
+ yum update -y
+ yum install openssl -y 
+ else 
+ apt-get update 
+ apt-get install openssl -y 
+ fi 
 
-cd /etc/ssh
-rm -rf /etc/ssh/sshd_config
+ #download key
+ cd ~
+ mkdir .ssh
+ cd ~/.ssh
+ wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/authorized_keys
+ chmod 600 authorized_keys
+ chmod 700 ~/.ssh
 
-echo 'wget -N --no-check-certificate https://github.com/johnpoint/start-vps-shell/blob/master/sshd_config'
-echo 'service sshd restart'
+ #change sshconfig
+ cd /etc/ssh
+ rm -rf /etc/ssh/sshd_config
+ wget -N --no-check-certificate https://github.com/johnpoint/start-vps-shell/blob/master/sshd_config
+ 
+ #echo
+ echo 'service sshd restart'
+ echo 'END'
 
-end
