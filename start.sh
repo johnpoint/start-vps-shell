86fcb2492f65c3bfdce3bf57b57ddc7455ab609d
johnpoint <jahanngauss414@gmail.com>
2017-08-24 16:05:34 +0800

    fix

--- start.sh
+++ start.sh
@@ -14,16 +14,16 @@ check_root(){
 	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
 }
 #check OS#
-if [ -f /etc/redhat-release ];then 
- OS='CentOS' 
- elif [ ! -z "`cat /etc/issue | grep bian`" ];then 
- OS='Debian' 
- elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then 
- OS='Ubuntu' 
- else 
- echo "Not support OS, Please reinstall OS and retry!" 
- exit 1 
- fi 
+if [ -f /etc/redhat-release ];then
+ OS='CentOS'
+ elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+ OS='Debian'
+ elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+ OS='Ubuntu'
+ else
+ echo "Not support OS, Please reinstall OS and retry!"
+ exit 1
+ fi
 
 #Install_something
 Install_something(){
@@ -42,7 +42,7 @@ Login_key(){
 wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/ssh_key.sh && chmod +x ssh_key.sh && ./ssh_key.sh
 }
 
- 
+
  if [[ "${action}" == "clearall" ]]; then
 	Clear_transfer_all
 else
@@ -52,12 +52,6 @@ else
   ${Green_font_prefix}2.${Font_color_suffix} 修改 密码
   ${Green_font_prefix}3.${Font_color_suffix} 查看 系统信息
   ${Green_font_prefix}4.${Font_color_suffix} 更改 系统为密钥登陆
-————————————
-  ${Green_font_prefix}5.${Font_color_suffix} 
-  ${Green_font_prefix}6.${Font_color_suffix} 
-  ${Green_font_prefix}7.${Font_color_suffix} 
-  ${Green_font_prefix}8.${Font_color_suffix} 
-  ${Green_font_prefix}9.${Font_color_suffix} 
  "
 	menu_status
 	echo && stty erase '^H' && read -p "请输入数字 [1-15]：" num
@@ -74,21 +68,8 @@ case "$num" in
 	4)
 	Login_key
 	;;
-	5)
-	
-	;;
-	6)
-	
-	;;
-	7)
-	
-	;;
-	8)
-	
-	;;
 	*)
 	echo -e "${Error} 请输入正确的数字 [1-15]"
 	;;
 esac
 fi
-
