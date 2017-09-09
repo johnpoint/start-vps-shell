f2527c43caea60915d17934dcd7f0267b0496f45
johnpoint <jahanngauss414@gmail.com>
2017-09-10 07:58:03 +0800

    update

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 2.0.1
+#	Version: 2.0.2
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver=2.0.1
+sh_ver=2.0.2
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 
 check_root(){
@@ -140,9 +140,112 @@ passwd
 Look_uname(){
 uname -a
 }
+#install openssl
+ Install_openssl(){
+ echo '正在安装openssl...'
+ if [[ ${OS} == 'CentOS' ]];then 
+ yum update -y
+ yum install openssl -y 
+ else 
+ apt-get update 
+ apt-get install openssl -y 
+ fi 
+ echo '安装完成！'
+ }
+ #Generate_key
+ Generate_key(){
+ echo '正在生成key'
+ ssh-keygen
+ echo '生成成功，保存于 /root/.ssh'
+ cd .ssh
+ mv id_rsa.pub authorized_keys
+ ls -a
+ chmod 600 authorized_keys
+ chmod 700 ~/.ssh
+ }
+ #modify_sshd_config
+ modify_sshd_config(){
+ echo '警告！此步骤如果出现异常请在 /root/sshd_config 目录处使用 mv 指令恢复配置文件'
+ echo '警告！此步骤如果出现异常请在 /root/sshd_config 目录处使用 mv 指令恢复配置文件'
+ echo '警告！此步骤如果出现异常请在 /root/sshd_config 目录处使用 mv 指令恢复配置文件'
+ echo '警告！此步骤如果出现异常请在 /root/sshd_config 目录处使用 mv 指令恢复配置文件'
+ mkdir ~/sshd_config
+ cp /etc/ssh/sshd_config /root/sshd_config
+ echo '请寻找RSAAuthentication yes PubkeyAuthentication yes 如不为yes 则改为yes'
+ echo '请寻找RSAAuthentication yes PubkeyAuthentication yes 如不为yes 则改为yes'
+ echo '请寻找RSAAuthentication yes PubkeyAuthentication yes 如不为yes 则改为yes'
+ vi /etc/ssh/sshd_config
+ echo '正在重启ssh服务'
+ echo '请使用key登陆测试是否成功'
+ }
+ #Download_key
+ Download_key(){
+ cd ~/.ssh
+ cat id_rsa
+ echo '把屏幕上面的密匙复制出来写入文件内，文件取名为 id_rsa （这个名称随意，但这个密匙文件一定要保存好！）。'
+ }
+ #Upload_key
+ Upload_key(){
+ mkdir ~/.ssh
+ if [[ ${OS} == 'CentOS' ]];then 
+ yum update -y
+ yum install lrzsz -y
+ else 
+ apt-get update 
+ apt-get install lrzsz -y
+ fi 
+ cd ~/.ssh
+ rz -y
+ ls -a
+ chmod 600 authorized_keys
+ chmod 700 ~/.ssh
+ }
+ #restart_sshd
+ restart_sshd(){
+ echo '正在重启ssh服务'
+ service ssh restart
+ service sshd restart
+ }
+ #close_passwd
+ close_passwd(){
+ echo '将PasswordAuthentication 改为no 并去掉#号'
+ echo '将PasswordAuthentication 改为no 并去掉#号'
+ echo '将PasswordAuthentication 改为no 并去掉#号'
+ echo '将PasswordAuthentication 改为no 并去掉#号'
+ vi /etc/ssh/sshd_config
+ echo '记得重启'
+ }
 #Login_key
 Login_key(){
-wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/ssh_key.sh && chmod +x ssh_key.sh && ./ssh_key.sh
+echo && echo -e "  你要做什么？
+	
+  ${Green_font_prefix}1.${Font_color_suffix} 安装 openssl
+  ${Green_font_prefix}2.${Font_color_suffix} 生成 key
+  ${Green_font_prefix}3.${Font_color_suffix} 取回 私钥
+  ${Green_font_prefix}4.${Font_color_suffix} 上传 key（多服务端单key选此项）
+  ${Green_font_prefix}5.${Font_color_suffix} 修改 sshd_config文件
+  ${Green_font_prefix}6.${Font_color_suffix} 重启 ssh服务
+  ${Green_font_prefix}7.${Font_color_suffix} 关闭 密码登陆
+  ——" && echo
+	stty erase '^H' && read -p "(默认: 取消):" Login_key_num
+	[[ -z "${Login_key_num}" ]] && echo "已取消..." && exit 1
+	if [[ ${Login_key_num} == "1" ]]; then
+		Install_openssl
+	elif [[ ${Login_key_num} == "2" ]]; then
+		Generate_key
+	elif [[ ${Login_key_num} == "3" ]]; then
+		Download_key
+	elif [[ ${Login_key_num} == "4" ]]; then
+		Upload_key
+	elif [[ ${Login_key_num} == "5" ]]; then
+		modify_sshd_config
+	elif [[ ${Login_key_num} == "6" ]]; then
+		restart_sshd
+	elif [[ ${Login_key_num} == "7" ]]; then
+		close_passwd
+	else
+		echo -e "${Error} 请输入正确的数字 [1-7]" && exit 1
+	fi
 }
 
 
