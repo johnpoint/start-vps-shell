2a81a87911f2981a85239a69b575b3ea78ef437a
johnpoint <jahanngauss414@gmail.com>
2017-09-16 21:16:05 +0800

    fix
    修复版本号

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ export PATH
 #	Author: johnpoint
 #=================================================
 
-sh_ver=3.0.1
+sh_ver="3.0.1"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
