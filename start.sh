3760112415903de799dc33d5e710b56bbedb29ef
johnpoint <jahanngauss414@gmail.com>
2017-09-17 13:42:08 +0800

    fix

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 3.2.1
+#	Version: 3.2.2
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="3.2.1"
+sh_ver="3.2.2"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -95,6 +95,7 @@ if [ -f /etc/redhat-release ];then
  #Install_status
  Install_status(){
  wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/status.sh && chmod +x status.sh
+ bash status.sh s
  }
  
  #Install_v2ray
