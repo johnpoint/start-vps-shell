dae685bcb3c9f57bd3e0d85f8e26a7cfaab80879
johnpoint <jahanngauss414@gmail.com>
2017-10-02 10:41:13 +0800

    update
    fix v2ray config '{'
    add install goflyway

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 4.0.0
+#	Version: 4.1.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="4.0.0"
+sh_ver="4.1.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -314,7 +314,7 @@ if [ -f /etc/redhat-release ];then
  
  
  
- echo " 
+ echo " {
  "log" : { 
  "access": "/var/log/v2ray/access.log", 
  "error": "/var/log/v2ray/error.log", 
@@ -381,7 +381,7 @@ if [ -f /etc/redhat-release ];then
  mv /etc/v2ray/config.json /etc/v2ray/config.back 
  mv config /etc/v2ray/config.json 
  rm /root/config.json 
- echo " 
+ echo "{
  "log": { 
  "loglevel": "info" 
  }, 
@@ -500,6 +500,10 @@ if [ -f /etc/redhat-release ];then
  Install_wordpress(){
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/wordpress.sh && chmod +x wordpress.sh && ./wordpress.sh
  }
+ #Install_GoFlyway
+ Install_GoFlyway(){
+ wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/goflyway.sh && chmod +x goflyway.sh && bash goflyway.sh
+ }
  #Install_something
 Install_something(){
 echo && echo -e "  你要做什么？
@@ -514,6 +518,7 @@ echo && echo -e "  你要做什么？
   ${Green_font_prefix}8.${Font_color_suffix} 安装/使用 youtube-dl
   ${Green_font_prefix}9.${Font_color_suffix} 安装微信互联系统（限Ubuntu）
   ${Green_font_prefix}10.${Font_color_suffix} 安装 wordpress博客
+  ${Green_font_prefix}11.${Font_color_suffix} 安装/管理 GoFlyway
   ——" && echo
 	stty erase '^H' && read -p "(默认: 取消):" install_num
 	[[ -z "${install_num}" ]] && echo "已取消..." && exit 1
@@ -537,6 +542,8 @@ echo && echo -e "  你要做什么？
 		Install_EFB
 	elif [[ ${install_num} == "10" ]]; then
 		Install_wordpress
+	elif [[ ${install_num} == "11" ]]; then
+		Install_GoFlyway
 	else
 		echo -e "${Error} 请输入正确的数字 [1-10]" && exit 1
 	fi
