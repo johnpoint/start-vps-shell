9a0b378f8f272294056a55658f3085ab88e86331
johnpoint <jahanngauss414@gmail.com>
2017-11-05 15:35:49 +0800

    #新增

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 5.0.0
+#	Version: 6.0.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="5.0.0"
+sh_ver="6.0.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -496,6 +496,9 @@ echo && echo -e "  你要做什么？
   ${Green_font_prefix}1.${Font_color_suffix} 安装 screen
   ${Green_font_prefix}2.${Font_color_suffix} 安装 lrzsz
   ${Green_font_prefix}3.${Font_color_suffix} 安装 git
+  ————————————————
+  ${Green_font_prefix}A.${Font_color_suffix} 以上全部安装
+  ————————————————
   ${Green_font_prefix}4.${Font_color_suffix} 安装/管理 ssr
   ${Green_font_prefix}5.${Font_color_suffix} 安装/管理 逗逼监控
   ${Green_font_prefix}6.${Font_color_suffix} 安装 V2ray
@@ -529,6 +532,10 @@ echo && echo -e "  你要做什么？
 		Install_wordpress
 	elif [[ ${install_num} == "11" ]]; then
 		Install_GoFlyway
+    elif [[ ${install_num} == "A" ]]; then
+		Install_screen
+		Install_lrzsz
+		Install_git
 	else
 		echo -e "${Error} 请输入正确的数字 [1-10]" && exit 1
 	fi
