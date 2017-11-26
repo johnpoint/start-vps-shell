d1e920364de22ae681e7e788f90356acbbc17517
johnpoint <jahanngauss414@gmail.com>
2017-11-26 16:09:06 +0800

    # update
    * 增加系统信息

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 7.0.0
+#	Version: 7.1.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="7.0.0"
+sh_ver="7.1.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -29,6 +29,40 @@ if [ -f /etc/redhat-release ];then
  echo "Not support OS, Please reinstall OS and retry!"
  exit 1
  fi
+
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+fi
+
+get_opsy() {
+    [ -f /etc/redhat-release ] && awk '{print ($1,$3~/^[0-9]/?$3:$4)}' /etc/redhat-release && return
+    [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+    [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
+}
+
+opsy=$( get_opsy )
+arch=$( uname -m )
+lbit=$( getconf LONG_BIT )
+kern=$( uname -r )
+
+check_bbr_status
+if [ $? -eq 0 ]; then
+	bbr="bbr未安装"
+	else
+	bbr="bbr已安装"
+    fi
 
 #Install_screen
  Install_screen(){
@@ -98,370 +132,7 @@ if [ -f /etc/redhat-release ];then
  bash status.sh s
  }
  #Install_v2ray
- Install_v2ray(){
- #Disable China
- wget http://iscn.kirito.moe/run.sh
- . ./run.sh
- if [[ $area == cn ]];then
- echo "Unable to install in china"
- exit
- fi
- # Get Public IP address
- ipc=$(ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1)
- if [[ "$IP" = "" ]]; then
- ipc=$(wget -qO- -t1 -T2 ipv4.icanhazip.com)
- fi
-
- uuid=$(cat /proc/sys/kernel/random/uuid)
-
- function Install(){
- #Install Basic Packages
- if [[ ${OS} == 'CentOS' ]];then
- yum install curl wget unzip ntp ntpdate -y
- else
- apt-get update
- apt-get install curl unzip ntp wget ntpdate -y
- fi
-
- #Set DNS
- echo "nameserver 8.8.8.8" > /etc/resolv.conf
- echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-
-
- #Update NTP settings
- rm -rf /etc/localtime
- ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
- ntpdate us.pool.ntp.org
-
- #Disable SELinux
- if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
- sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
- setenforce 0
- fi
-
- #Run Install
- cd /root
-
- bash <(curl -L -s https://install.direct/go.sh)
-
- }
-
- clear
- echo 'V2Ray 一键安装|配置脚本 Author：Kirito && 雨落无声'
-
- echo ''
- echo '此脚本会关闭iptables防火墙，切勿用于生产环境！'
-
- while :; do echo
- read -p "输入用户等级（自用请输入1，共享请输入0）:" level
- if [[ ! $level =~ ^[0-1]$ ]]; then
- echo "${CWARNING}输入错误! 请输入正确的数字!${CEND}"
- else
- break
- fi
- done
-
- echo ''
-
- read -p "输入主要端口（默认：32000）:" mainport
- [ -z "$mainport" ] && mainport=32000
-
- echo ''
-
- read -p "是否启用HTTP伪装?（默认开启） [y/n]:" ifhttpheader
- [ -z "$ifhttpheader" ] && ifhttpheader='y'
- if [[ $ifhttpheader == 'y' ]];then
- httpheader=',
- "streamSettings": {
- "network": "tcp",
- "tcpSettings": {
- "connectionReuse": true,
- "header": {
- "type": "http",
- "request": {
- "version": "1.1",
- "method": "GET",
- "path": ["/"],
- "headers": {
- "Host": ["www.baidu.com", "www.sogou.com/"],
- "User-Agent": [
- "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36",
- "Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.109 Mobile/14A456 Safari/601.1.46"
- ],
- "Accept-Encoding": ["gzip, deflate"],
- "Connection": ["keep-alive"],
- "Pragma": "no-cache"
- }
- },
- "response": {
- "version": "1.1",
- "status": "200",
- "reason": "OK",
- "headers": {
- "Content-Type": ["application/octet-stream", "application/x-msdownload", "text/html", "application/x-shockwave-flash"],
- "Transfer-Encoding": ["chunked"],
- "Connection": ["keep-alive"],
- "Pragma": "no-cache"
- }
- }
- }
- }
- }'
- else
- httpheader=''
- read -p "是否启用mKCP协议?（默认开启） [y/n]:" ifmkcp
- [ -z "$ifmkcp" ] && ifmkcp='y'
- if [[ $ifmkcp == 'y' ]];then
- mkcp=',
- "streamSettings": {
- "network": "kcp"
- }'
- else
- mkcp=''
- fi
- fi
-
- echo ''
-
- read -p "是否启用动态端口?（默认开启） [y/n]:" ifdynamicport
- [ -z "$ifdynamicport" ] && ifdynamicport='y'
- if [[ $ifdynamicport == 'y' ]];then
-
- read -p "输入数据端口起点（默认：32001）:" subport1
- [ -z "$subport1" ] && subport1=32000
-
- read -p "输入数据端口终点（默认：32500）:" subport2
- [ -z "$subport2" ] && subport2=32500
-
- read -p "输入每次开放端口数（默认：10）:" portnum
- [ -z "$portnum" ] && portnum=10
-
- read -p "输入端口变更时间（单位：分钟）:" porttime
- [ -z "$porttime" ] && porttime=5
- dynamicport="
- \"inboundDetour\": [
- {
- \"protocol\": \"vmess\",
- \"port\": \"$subport1-$subport2\",
- \"tag\": \"detour\",
- \"settings\": {},
- \"allocate\": {
- \"strategy\": \"random\",
- \"concurrency\": $portnum,
- \"refresh\": $porttime
- }${mkcp}${httpheader}
- }
- ],
- "
- else
- dynamicport=''
- fi
-
- echo ''
-
- read -p "是否启用 Mux.Cool?（默认开启） [y/n]:" ifmux
- [ -z "$ifmux" ] && ifmux='y'
- if [[ $ifmux == 'y' ]];then
- mux=',
- "mux": {
- "enabled": true
- }
- '
- else
- mux=""
- fi
-
- while :; do echo
- echo '1. HTTP代理（默认）'
- echo '2. Socks代理'
- read -p "请选择客户端代理类型: " chooseproxytype
- [ -z "$chooseproxytype" ] && chooseproxytype=1
- if [[ ! $chooseproxytype =~ ^[1-2]$ ]]; then
- echo '输入错误，请输入正确的数字！'
- else
- break
- fi
- done
-
- if [[ $chooseproxytype == 1 ]];then
- proxytype='http'
- else 
- proxytype='socks'
- fi
- #CheckIfInstalled
- if [ ! -f "/usr/bin/v2ray/v2ray" ]; then
- Install
- fi
-
- #Disable iptables
- iptables -P INPUT ACCEPT
- iptables -P FORWARD ACCEPT
- iptables -P OUTPUT ACCEPT
- iptables -F
- echo " {
- "log" : {
- "access": "/var/log/v2ray/access.log",
- "error": "/var/log/v2ray/error.log",
- "loglevel": "warning"
- },
- "inbound": {
- "port": $mainport,
- "protocol": "vmess",
- "settings": {
- "clients": [
- {
- "id": "$uuid",
- "level": $level,
- "alterId": 100
- }
- ]
- }${mkcp}${httpheader}
- },
- "outbound": {
- "protocol": "freedom",
- "settings": {}
- },
-
- ${dynamicport}
-
- "outboundDetour": [
- {
- "protocol": "blackhole",
- "settings": {},
- "tag": "blocked"
- }
- ],
- "routing": {
- "strategy": "rules",
- "settings": {
- "rules": [
- {
- "type": "field",
- "ip": [
- "0.0.0.0/8",
- "10.0.0.0/8",
- "100.64.0.0/10",
- "127.0.0.0/8",
- "169.254.0.0/16",
- "172.16.0.0/12",
- "192.0.0.0/24",
- "192.0.2.0/24",
- "192.168.0.0/16",
- "198.18.0.0/15",
- "198.51.100.0/24",
- "203.0.113.0/24",
- "::1/128",
- "fc00::/7",
- "fe80::/10"
- ],
- "outboundTag": "blocked"
- }
- ]
- }
- }
- }
- ">config
- rm -rf /etc/v2ray/config.back
- mv /etc/v2ray/config.json /etc/v2ray/config.back
- mv config /etc/v2ray/config.json
- rm /root/config.json
- echo "{
- "log": {
- "loglevel": "info"
- },
- "inbound": {
- "port": 1080,
- "listen": "127.0.0.1",
- "protocol": "$proxytype",
- "settings": {
- "auth": "noauth",
- "udp": true,
- "ip": "127.0.0.1"
- }
- },
- "outbound": {
- "protocol": "vmess",
- "settings": {
- "vnext": [
- {
- "address": "$ipc",
- "port": $mainport,
- "users": [
- {
- "id": "$uuid",
- "alterId": 100
- }
- ]
- }
- ]
- }${mkcp}${httpheader}${mux}
- },
- "outboundDetour": [
- {
- "protocol": "freedom",
- "settings": {},
- "tag": "direct"
- }
- ],
- "dns": {
- "servers": [
- "8.8.8.8",
- "8.8.4.4",
- "localhost"
- ]
- },
- "routing": {
- "strategy": "rules",
- "settings": {
- "rules": [
- {
- "type": "chinasites",
- "outboundTag": "direct"
- },
- {
- "type": "field",
- "ip": [
- "0.0.0.0/8",
- "10.0.0.0/8",
- "100.64.0.0/10",
- "127.0.0.0/8",
- "169.254.0.0/16",
- "172.16.0.0/12",
- "192.0.0.0/24",
- "192.0.2.0/24",
- "192.168.0.0/16",
- "198.18.0.0/15",
- "198.51.100.0/24",
- "203.0.113.0/24",
- "::1/128",
- "fc00::/7",
- "fe80::/10"
- ],
- "outboundTag": "direct"
- },
- {
- "type": "chinaip",
- "outboundTag": "direct"
- }
- ]
- }
- }
- } ">/root/config.json
- service v2ray start
- clear
- #INstall Success
- echo 'Telegram Group: https://t.me/functionclub'
- echo 'Google Puls: https://plus.google.com/communities/113154644036958487268'
- echo 'Github: https://github.com/FunctionClub'
- echo 'QQ Group:*********'
- echo 'Function Club 无限期停更说明'
- echo 'https://www.ixh.me/2017/05/function-club-stop/'
- echo '教程地址：https://github.com/FunctionClub/V2ray-Bash/blob/master/README.md'
- echo '配置完成，客户端配置文件在 /root/config.json'
- echo ''
- echo "程序主端口：$mainport"
- echo "UUID: $uuid"
- }
+ 
  #Install_sync
  Install_sync(){
  wget -N --no-check-certificate https://raw.githubusercontent.com/johnpoint/start-vps-shell/master/sync.sh && chmod +x sync.sh && ./sync.sh
@@ -493,6 +164,19 @@ if [ -f /etc/redhat-release ];then
  Install_ExpressBot(){
  wget -N --no-check-certificate https://raw.githubusercontent.com/BennyThink/ExpressBot/master/install.sh && chmod +x install.sh && ./install.sh
  }
+ #Install_bbr
+ Install_bbr(){
+ wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+ }
+ #check_bbr_status() {
+ check_bbr_status() {
+    local param=$(sysctl net.ipv4.tcp_available_congestion_control | awk '{print $3}')
+    if [[ "${param}" == "bbr" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
  #Install_something
 Install_something(){
 echo && echo -e "  你要做什么？
@@ -512,6 +196,7 @@ echo && echo -e "  你要做什么？
   ${Green_font_prefix}10.${Font_color_suffix} 安装 wordpress博客
   ${Green_font_prefix}11.${Font_color_suffix} 安装/管理 GoFlyway
   ${Green_font_prefix}12.${Font_color_suffix} 安装/管理 ExpressBot
+  ${Green_font_prefix}13.${Font_color_suffix} 安装 bbr
   ————————————————" && echo
 	stty erase '^H' && read -p "(默认: 取消):" install_num
 	[[ -z "${install_num}" ]] && echo "已取消..." && exit 1
@@ -537,12 +222,14 @@ echo && echo -e "  你要做什么？
 		Install_wordpress
 	elif [[ ${install_num} == "11" ]]; then
 		Install_GoFlyway
-    elif [[ ${install_num} == "A" ]]; then
+	elif [[ ${install_num} == "A" ]]; then
 		Install_screen
 		Install_lrzsz
 		Install_git
 	elif [[ ${install_num} == "12" ]]; then
 	Install_ExpressBot
+	elif [[ ${install_num} == "13" ]]; then
+	Install_bbr
 	else
 		echo -e "${Error} 请输入正确的选项" && exit 1
 	fi
@@ -713,11 +400,19 @@ echo -e "当前版本为 [ ${sh_ver} ]，开始检测最新版本..."
 		echo -e "当前已是最新版本[ ${sh_new_ver} ] !"
 	fi
 
+
  if [[ "${action}" == "clearall" ]]; then
 	Clear_transfer_all
 else
 	echo -e "  VPS一键管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
   ---- johnpoint ----
+  
+  =============== System Information ===================
+  =		OS      : $opsy
+  =		Arch    : $arch ($lbit Bit)
+  =		Kernel  : $kern
+  =		BBR     : $bbr
+  =============== System Information ===================
   ${Green_font_prefix}1.${Font_color_suffix} 安装 软件
   ${Green_font_prefix}2.${Font_color_suffix} 修改 密码
   ${Green_font_prefix}3.${Font_color_suffix} 查看 系统信息
