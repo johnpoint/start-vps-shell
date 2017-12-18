85c5e54259dfe2e6f6f3690129e1c4f9e7560e84
johnpoint <jahanngauss414@gmail.com>
2017-12-18 21:20:22 +0800

    # update * add check bbr stauts

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 8.1.4
+#	Version: 8.2.0
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="8.1.4"
+sh_ver="8.2.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -42,6 +42,20 @@ elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     PM='yum'
 fi
 
+#check_bbr
+check_bbr(){
+check_bbr_status_on=`sysctl net.ipv4.tcp_available_congestion_control | awk '{print $3}'`
+	if [[ "${check_bbr_status_on}" = "bbr" ]]; then
+		# 检查是否启动BBR
+		check_bbr_status_off=`lsmod | grep bbr`
+		if [[ "${check_bbr_status_off}" = "" ]]; then
+			bbr=BBR 已开启但未正常启动
+		else
+			bbr=BBR 已开启并已正常启动
+		fi
+	fi
+}
+
 #check_IP_address
 check_IP_address(){
 curl http://members.3322.org/dyndns/getip 
@@ -57,6 +71,8 @@ arch=$( uname -m )
 lbit=$( getconf LONG_BIT )
 kern=$( uname -r )
 ip=$(check_IP_address)
+check_bbr
+
 #Install_screen
  Install_screen(){
  echo -e "${Info} 正在安装screen..."
@@ -359,6 +375,7 @@ else
   =         OS       : $opsy
   =		Arch     : $arch ($lbit Bit)
   =		Kernel  : $kern
+  =       BBR       : $bbr
   =============== System Information ===================
   ${Green_font_prefix}1.${Font_color_suffix} 安装 软件
   ${Green_font_prefix}2.${Font_color_suffix} 修改 密码
