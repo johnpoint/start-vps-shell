b96dea415999208e1825835e6986bc8f9d3ff295
johnpoint <jahanngauss414@gmail.com>
2017-12-19 10:01:27 +0800

    # fix

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 8.3.3
+#	Version: 8.3.4
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #=================================================
 
-sh_ver="8.3.3"
+sh_ver="8.3.4"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -41,6 +41,8 @@ elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
     release="centos"
     PM='yum'
 fi
+
+Update_shell
 
 #check_bbr
 check_bbr_status_on=`sysctl net.ipv4.tcp_available_congestion_control | awk '{print $3}'`
