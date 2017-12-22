cfad5bf4988f5fe9232b8e1e38c4331e91e3ea96
johnpoint <jahanngauss414@gmail.com>
2017-12-22 12:14:23 +0800

    update

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 8.4.5
+#	Version: 8.4.6
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="8.4.5"
+sh_ver="8.4.6"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -93,7 +93,7 @@ check_bbr_status_on=`sysctl net.ipv4.tcp_available_congestion_control | awk '{pr
 
 #check_IP_address
 check_IP_address(){
-curl ip.cn
+curl -s ip.cn
 }
 
 get_opsy() {
