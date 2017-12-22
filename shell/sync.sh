76a1f3c0c0f14a8a92fb80f2e60e6f5a7bd5dc93
johnpoint <jahanngauss414@gmail.com>
2017-12-22 10:01:26 +0800

    # update

--- shell/sync.sh
+++ shell/sync.sh
@@ -4,11 +4,11 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 1.0.1
+#	Version: 1.0.2
 #	Author: johnpoint
 #=================================================
 
-sh_ver="1.0.1"
+sh_ver="1.0.2"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -16,7 +16,7 @@ Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 Separator_1="——————————————————————————————"
 
 check_ip(){
-curl ip.cn
+curl ifconfig.me
 }
 
 #Install_sync
