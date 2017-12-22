408ae1372d3d93fdeb47d911f16fb27c2c8bdf26
johnpoint <jahanngauss414@gmail.com>
2017-12-22 13:17:58 +0800

    # update

--- shell/ip.sh
+++ shell/ip.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 0.0.2
+#	Version: 0.0.3
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="0.0.2"
+sh_ver="0.0.3"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -21,6 +21,33 @@ check_root(){
 	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
 }
 
+#check OS
+if [ -f /etc/redhat-release ]; then
+    release="centos"
+    PM='yum'
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+    PM='apt-get'
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    PM='apt-get'
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    PM='yum'
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+    PM='apt-get'
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+    PM='apt-get'
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+    PM='yum'
+fi
+
+${PM} update
+${PM} install jq -y
+
 config=$( curl -s ipinfo.io )
 touch ip.json
 echo "
@@ -28,25 +55,24 @@ echo "
 $config" > ip.json
 
 ip(){
-jq 'ip' ip.json
+jq '.ip' ip.json
 }
 
 city(){
-jq 'city' ip.json
+jq '.city' ip.json
 }
 
 country(){
-jq 'country' ip.json
+jq '.country' ip.json
 }
 
 loc(){
-jq 'loc' ip.json
+jq '.loc' ip.json
 }
 
 org(){
-jq 'org' ip.json
+jq '.org' ip.json
 }
-
 
 ip=$(ip)
 city=$(city)
