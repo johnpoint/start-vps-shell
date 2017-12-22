114c65a4b8ad5229d7399d2b534c2d12d1e22ca8
johnpoint <jahanngauss414@gmail.com>
2017-12-22 11:17:26 +0800

    test

--- shell/ip.sh
+++ shell/ip.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 0.0.1
+#	Version: 0.0.2
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="0.0.1"
+sh_ver="0.0.2"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -21,30 +21,25 @@ check_root(){
 	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
 }
 
-ipconfig=$( curl ipinfo.io )
-
-echo "$ipconfig" > ipconfig.txt
-
 ip(){
- grep -Po '"ip":".*?"' | grep -Po '\d+' ipconfig.txt
+curl -s 'ipconfig.io' | jq 'ip'
 }
 
 city(){
- grep -Po '"city":".*?"' | grep -Po '\d+' ipconfig.txt
+curl -s 'ipconfig.io' | jq 'city'
 }
 
 country(){
- grep -Po '"country":".*?"' | grep -Po '\d+' ipconfig.txt
+curl -s 'ipconfig.io' | jq 'country'
 }
 
 loc(){
-grep -Po '"loc":".*?"' | grep -Po '\d+' ipconfig.txt
+curl -s 'ipconfig.io' | jq 'loc'
 }
 
 org(){
- grep -Po '"org":".*?"' | grep -Po '\d+' ipconfig.txt
+curl -s 'ipconfig.io' | jq 'org'
 }
-
 
 
 ip=$(ip)
@@ -55,7 +50,7 @@ org=$(org)
 
 echo -e "
 ip概况
-IP地址：$(ip)
-位置：$(country) $(city) $(loc)
-组织：$(org)
+IP地址：$ip
+位置：$country $city $loc
+组织：$org
 "
