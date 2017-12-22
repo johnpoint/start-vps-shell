ed030ce8835d4bbd2e2df3132686503db91611bc
johnpoint <jahanngauss414@gmail.com>
2017-12-22 12:21:00 +0800

    update

--- shell/ip.sh
+++ shell/ip.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 0.0.2
+#	Version: 0.0.1
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="0.0.2"
+sh_ver="0.0.1"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -22,24 +22,25 @@ check_root(){
 }
 
 ip(){
-curl -s 'ipconfig.io' | jq 'ip'
+curl -s 'ipinfo.io' grep -Po '"ip":".*?"' | grep -Po '\d+'
 }
 
 city(){
-curl -s 'ipconfig.io' | jq 'city'
+curl -s 'ipinfo.io' grep -Po '"city":".*?"' | grep -Po '\d+' ipconfig.txt
 }
 
 country(){
-curl -s 'ipconfig.io' | jq 'country'
+curl -s 'ipinfo.io' grep -Po '"country":".*?"' | grep -Po '\d+' ipconfig.txt
 }
 
 loc(){
-curl -s 'ipconfig.io' | jq 'loc'
+curl -s 'ipinfo.io' grep -Po '"loc":".*?"' | grep -Po '\d+' ipconfig.txt
 }
 
 org(){
-curl -s 'ipconfig.io' | jq 'org'
+curl -s 'ipinfo.io' grep -Po '"org":".*?"' | grep -Po '\d+' ipconfig.txt
 }
+
 
 
 ip=$(ip)
