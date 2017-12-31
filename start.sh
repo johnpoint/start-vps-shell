24e54f240df8f15a0e6a2f4c54ad030951011502
johnpoint <jahanngauss414@gmail.com>
2017-12-31 22:02:44 +0800

    # fix

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 8.6.1
+#	Version: 8.6.2
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="8.6.1"
+sh_ver="8.6.2"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -460,4 +460,3 @@ case "$num" in
 	;;
  esac
  fi
- done
