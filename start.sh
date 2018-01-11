45e2d4e45d814c19e8e6e5bb1a3416a6363597c4
johnpoint <jahanngauss414@gmail.com>
2018-01-11 16:13:50 +0800

    # debug

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 2.2.0-4
+#	Version: 2.2.0-5
 #	Blog: blog.lvcshu.club
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.0-4"
+sh_ver="2.2.0-5"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -174,11 +174,6 @@ rm -rf ip.json
 Install_depend_now(){
 # ${PM} update >/dev/null
  ${PM} install ${depend_name}
-if  [ ! -e '/usr/bin/${depend_name}' ]; then
-        echo -e "${Error}${depend_nane}未能成功安装，有可能是包名错误，请确认!"
-	else
-		echo -e "${Info} ${depend_name} 安装完成"
-fi
 }
  
  ############
