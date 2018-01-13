fbca3fc5086a7d0666b1f874d32d52855f0ef65b
johnpoint <jahanngauss414@gmail.com>
2018-01-13 11:23:01 +0800

    # fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 2.2.0-10
+#	Version: 2.2.0-11
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #	Email: jahanngauss414@gmail.com
@@ -12,7 +12,7 @@ export PATH
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.0-10"
+sh_ver="2.2.0-11"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -124,6 +124,8 @@ Update_shell(){
 			fi
 			echo -e "脚本已更新为最新版本[ ${Green_font_prefix}${sh_new_ver}${Font_color_suffix} ] !"
             chmod +x start.sh
+            ./start.sh
+            exit
 		else
 			echo && echo "	已取消..." && echo
 		fi
