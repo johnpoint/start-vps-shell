b0bbb92df8d76d13c0d887cac07a11878631fbdc
johnpoint <jahanngauss414@gmail.com>
2018-01-17 20:49:13 +0800

    # fix

--- shell/v2ray/v2ray.sh
+++ shell/v2ray/v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 1.1.1
+#	Version: 1.2.0
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="1.1.1"
+sh_ver="1.2.0"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -91,6 +91,10 @@ service v2ray stop
 Restart(){
 Stop
 Start
+}
+
+Status(){
+service v2ray status
 }
  
  Install_main(){
@@ -518,6 +522,7 @@ echo -e "${Info}配置程序执行完毕~"
 Start
 }
 
+Update_shell
 clear
 echo  -e "v2ray安装/管理脚本 [v${Green_font_prefix}${sh_ver}${Font_color_suffix}]
 ———— Author:johnpoint ————
