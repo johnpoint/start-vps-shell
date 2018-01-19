d91139e30bcf6fd1b75a83cddab8ce26763d7ced
johnpoint <jahanngauss414@gmail.com>
2018-01-19 22:00:32 +0800

    # fix

--- shell/v2ray/v2ray.sh
+++ shell/v2ray/v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 1.5.15
+#	Version: 1.5.16
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="1.5.15"
+sh_ver="1.5.16"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -260,8 +260,8 @@ DynamicPort(){
     }
   ],"
 detour=',
-      \"detour\": {        
-        \"to\": \"dynamicPort\"   
+      "detour": {        
+        "to": "dynamicPort"   
       }'
  else 
  movePort='' 
