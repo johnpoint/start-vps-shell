a38d4fa21764dca861b756995f78842259176ce1
johnpoint <jahanngauss414@gmail.com>
2018-01-19 14:02:45 +0800

    fix

--- shell/v2ray/v2ray.sh
+++ shell/v2ray/v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 1.5.9
+#	Version: 1.5.10
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="1.5.9"
+sh_ver="1.5.10"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -405,7 +405,7 @@ echo "
 	\"ip\":\"${ip}\",
 	\"port\":\"${main_port}\",
 	\"move\":\"${port1} ~ ${port2}\",
-	\"mux\":\"${ifmux}\",
+	\"mux\":\"${mux}\",
 	\"proxy\":\"${proxy}\",
 	\"user\":\"${username}\",
 	\"passwd\":\"${pw}\",
@@ -442,7 +442,7 @@ ifmux=$( cat /etc/v2ray/sh_config.json | jq -r '.ifmux' )
 		范围：	${move}
 		刷新频率：	${refresh}	分钟
 		同时开放	${Green_font_prefix}${port_num}${Font_color_suffix}	端口
-	Mux.Cool多路复用：${Green_font_prefix}${mux}${ifmux}${Font_color_suffix}
+	Mux.Cool多路复用：${Green_font_prefix}${mux}${Font_color_suffix}
 	用户配置路径：/etc/v2ray/user_config.json
 	——————————————————————"
 elif [[ ${type} == '1' ]]; then
