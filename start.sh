23bdcec85e68ea3c1f392b67bf8ff1ab928ca190
johnpoint <jahanngauss414@gmail.com>
2018-02-21 14:10:49 +0800

    # fix

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ export PATH
 
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Version: 2.4.1
+#	Version: 2.4.2
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #	Email: jahanngauss414@gmail.com
@@ -12,7 +12,7 @@ export PATH
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.4.1"
+sh_ver="2.4.2"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -260,7 +260,7 @@ Install_depend_now(){
  }
  
  Base64(){
- wget -q https://github.com/johnpoint/start-vps-shell/raw/master/shell/base64.sh chmod +x base64.sh && ./base64.sh
+ wget -q https://github.com/johnpoint/start-vps-shell/raw/master/shell/base64.sh && chmod +x base64.sh && ./base64.sh
  }
  
 ##############
